e2a64c8bcb50d46c73ec5e7d5e31d1b61e9ab3d9
Evgen Tikhonov <tihonovaelena1503@gmail.com>
2019-03-15 21:45:48 +0300

    Same for 2 numbers

---
+++ same.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num_1, num_2, num_3;
+    cout << "Enter 3 numbers" << endl;
+    cin >> num_1 >> num_2 >> num_3;
+
+    if (num_1 == num_2 && num_1 == num_3 && num_2 == num_3) {
+        cout << num_1 << " = " << num_2 << " = " << num_3 << " (3 same numbers) " << endl;
+    }
+
+    else if (num_1 == num_2 && num_1 != num_3 && num_2 != num_3) {
+        cout << num_1 << " = " << num_2 << " (2 same numbers) " << endl;
+    }
+
+    return 0;
+}
