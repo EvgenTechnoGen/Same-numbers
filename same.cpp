b948ee6ccae08b97a546dca00807d8d307545b90
Evgen Tikhonov <tihonovaelena1503@gmail.com>
2019-03-15 22:08:20 +0300

    Update same.cpp

--- same.cpp
+++ same.cpp
@@ -2,17 +2,25 @@
 using namespace std;
 
 int main() {
-    int num_1, num_2, num_3;
-    cout << "Enter 3 numbers" << endl;
-    cin >> num_1 >> num_2 >> num_3;
+   int num_1, num_2, num_3;
+   cout << "Enter 3 numbers:" << endl;
+   cin >> num_1 >> num_2 >> num_3;
 
-    if (num_1 == num_2 && num_1 == num_3 && num_2 == num_3) {
-        cout << num_1 << " = " << num_2 << " = " << num_3 << " (3 same numbers) " << endl;
-    }
+   if (num_1 == num_2 && num_1 == num_3 && num_2 == num_3) {
+       cout << num_1 << " = " << num_2 << " = " << num_3 << " (3 same numbers) " << endl;
+   }
 
-    else if (num_1 == num_2 && num_1 != num_3 && num_2 != num_3) {
-        cout << num_1 << " = " << num_2 << " (2 same numbers) " << endl;
-    }
+   else if (num_1 == num_2 && num_1 != num_3 && num_2 != num_3) {
+       cout << num_1 << " = " << num_2 << " (2 same numbers) " << endl;
+   }
 
-    return 0;
+   else if (num_1 != num_2 && num_2 != num_3 && num_1 == num_3) {
+       cout << num_1 << " = " << num_3 << " (2 same numbers) " << endl;
+   }
+
+   else if (num_1 != num_2 && num_1 != num_3 && num_2 != num_3) {
+       cout << "No same numbers." << endl;
+   }
+
+   return 0;
 }
